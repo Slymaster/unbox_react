5bc447d386d7883ab298bce62c11cb8bd849da92
Slymaster <sylvain.rey.pro@gmail.com>
2021-11-29 11:37:04 +0100

    try to add github pages

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,33 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
+      }
+    }
+  },
+  mocha: {
+    timeout: 100000
+  },
+  compilers: {
+    solc: {
+      version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+         enabled: false,
+         runs: 200
+        },
+      }
     }
   }
 };
